fa6675d1bd500a5f1f66af27b965d4766b047005
Andres <andrestupiza@gmail.com>
2022-06-17 19:19:59 +0200

    He conseguido que me lea los ficheros y me muestre solo las columanas que le indico

--- src/main/java/ejercicio1/Farolas.java
+++ src/main/java/ejercicio1/Farolas.java
@@ -10,32 +10,49 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 /**
  *
- * @author DAW
+ * @author Ramiro Andres T
  */
 public class Farolas {
-     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String ruta;
-        BufferedReader br = null;
-        String cvsSplitBy = ";";
-        String line = "";
-        ArrayList<String[]> contenido = new ArrayList<String[]>();
+    
+     public static void main(String[] args) throws FileNotFoundException, IOException {  //Controlamos que nos salte una excepción si el fichero no ha sido encontrado 
+        //Creamos nuestras variables, bufferedReader y nuestro HashMap que vamos ha utillizar en el progrma
         
-      
-        ruta = "./20220408_DATOS_ABIERTOS_UNIDAD_LUMINOSA_.csv";
+        String line = null;
+        BufferedReader br = null;
+        BufferedReader br1 = null;
+        String line1 = null;
+        String cvsSplitBy = ";";
+        String cvsSplitBy1 = ";";
+        Map <String, String>listaMap=new HashMap<>();
         try {
-            br = new BufferedReader(new FileReader(ruta));
-
-            while ((line = br.readLine()) != null) {
-
-                String[] datos = line.split(cvsSplitBy);
-                //Imprime datos.
-                System.out.println(datos[2] + "; " + datos[9]);
-
-            }
+        //br sera quien contenga la ubicación de nuestro archivo Datos abiertos unidad luminosa 
+        br = new BufferedReader( new FileReader ("C:/Users/DAW/Desktop/ArchivosexamenExtraordinarioPROG/20220408_DATOS_ABIERTOS_UNIDAD_LUMINOSA_.csv"));
+        //br1 sera quien contega la ubicacion de nuestro archivo Dstritos 
+        br1 = new BufferedReader( new FileReader ("C:/Users/DAW/Desktop/ArchivosexamenExtraordinarioPROG/Distritos.csv"));
+        
+        //  while ((line = br.readLine()) != null && (line = br.readLine()) != null  ) { ------> queria hacer esto para que me muestre tanto lo del el contenido de br y br1 pero solo me muestra algunos valores
+        while ((line = br.readLine()) != null) { //Como no es Null le indicamos que lea las lineas de las dos rutas 
+            //Almacenamos los valores leido en datos(las lineas leidas de br) y en datos1 (almacenamos las lineas leidas de br1)
+            String[] datos = line.split(cvsSplitBy);
+            String[] datos1 = line.split(cvsSplitBy1);
+            System.out.println(datos[1]+ " ; " + datos[8]);
+            
+            
+            //Guardamos los datos que hemos leido en el HashMap  expecificando que solo queremos los datos de la posicin dentro de Array  1(tipo de alumbrado) y 6(Distrito ) 
+            //Lo que pasa es que al guardardo en el HashMap me guarda todo lo que va desde la posicion del tipo de farola al distrito 
+            for (int i = 0; i < line.length(); i++) {
+                    listaMap.put(datos[1], datos[8]);
+                }
+           
+                    //System.out.println(listaMap);
+             } 
           //Excepciones 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
