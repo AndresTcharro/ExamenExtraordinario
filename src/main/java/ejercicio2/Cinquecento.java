450b1f81026ba3d32ad550f1e238fb63c0182670
Andres <andrestupiza@gmail.com>
2022-06-17 20:18:22 +0200

    FIN :)

--- src/main/java/ejercicio2/Cinquecento.java
+++ src/main/java/ejercicio2/Cinquecento.java
@@ -20,19 +20,25 @@ public class Cinquecento {
      
 try {
  int numeroCasos ;
-     int año;
+      int año;
+      int siglo;
       //Scanner para perdir al usuario que introduzca un nuemero
       Scanner entrada  = new Scanner(System.in);
-      System.out.println("Introduce el numero de casos de los que quieres saber el siglo");
+      System.out.println("Introduce el número de casos de los que quieres saber el siglo:");
       numeroCasos = entrada.nextInt();
-      System.out.println("Introduce el año del cual quieres saber el siglo");
+      System.out.println("Introduce el año del cual quieres saber el siglo:");
       //Lo que hacemos "numeroCasos-- != 0" es que  Compruebe que numeroCasos no es igual a cero antes 
       //de realizarle un decremento, hasta llegar a la psocion 3 donde se para el programa 
       while (numeroCasos-- != 0) {
       //agrego en año la entra por teclado 
         año = entrada.nextInt();
         
-        System.out.println((año / 100) + (año % 100 != 0 ? 1 : 0));
+        //Math.floorMod(año,100) actua como el modulo el resto de una división entre enteros es distinto a cero (condicion)
+        // 1 es verdadero y 0 seria falsso siendo asi que se sumaria a la division 
+        //que hemos realizado previamente 
+        siglo = (año / 100) + (Math.floorMod(año,100) != 0 ? 1 : 0);
+        
+        System.out.println(siglo);
       }
         
             //Excepciones 
